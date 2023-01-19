fc10812d0ee21f33707bfb0ed09de83bca8c4b67
Jared Silver <jrsilver88@gmail.com>
2023-01-19 12:52:25 -0800

    created routes folder with users, profile, posts, and auth basic routes.

---
+++ routes/api/posts.js
@@ -0,0 +1,11 @@
+import express from "express";
+const router = express.Router();
+
+// @route  GET api/posts
+// @desc   Test route
+// @access Public
+router.get("/", (req, res) => {
+  res.send("Post route");
+});
+
+export default router;
--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 import express from "express";
 import mongoose from "mongoose";
 import connectDB from "./config/db.js";
+import userRoutes from "./routes/api/users.js";
+import authRoutes from "./routes/api/auth.js";
+import profileRoutes from "./routes/api/profile.js";
+import postRoutes from "./routes/api/posts.js";
 const app = express();
 
 // Connect Database
@@ -10,6 +14,12 @@ mongoose.set("strictQuery", false);
 app.get("/", (req, res) => {
   res.send("API Running");
 });
+
+// Define Routes
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/profile", profileRoutes);
+app.use("/api/posts", postRoutes);
 
 const PORT = process.env.PORT || 5000;
 
