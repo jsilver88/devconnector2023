88da67f9763b4eeb6d46f42af85541eaa7669b37
Jared Silver <jrsilver88@gmail.com>
2023-01-19 23:09:30 -0800

    created an auth middleware that gives access to a user if they have a valid token. set up auth route to find a user by token

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,11 +1,19 @@
 import express from "express";
+import auth from "../../middleware/auth.js";
+import User from "../../models/User.js";
 const router = express.Router();
 
 // @route  GET api/auth
 // @desc   Test route
 // @access Public
-router.get("/", (req, res) => {
-  res.send("Auth route");
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server error");
+  }
 });
 
 export default router;
