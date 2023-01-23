ed885bccb9d9b87bb3e8167d64b166a2df89e48c
Jared Silver <jrsilver88@gmail.com>
2023-01-22 22:43:38 -0800

    created the posts model and user can add posts

---
+++ models/Post.js
@@ -0,0 +1,55 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "Users",
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+  },
+  avatar: {
+    type: String,
+  },
+  likes: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "Users",
+      },
+    },
+  ],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "Users",
+      },
+      text: {
+        type: String,
+        required: true,
+      },
+      name: {
+        type: String,
+      },
+      avatar: {
+        type: String,
+      },
+      date: {
+        type: Date,
+        default: Date.now(),
+      },
+    },
+  ],
+  date: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+export default mongoose.model("post", PostSchema);
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,11 +1,41 @@
 import express from "express";
+import { check, validationResult } from "express-validator";
+import auth from "../../middleware/auth.js";
+import User from "../../models/User.js";
+import Post from "../../models/Post.js";
+import Profile from "../../models/Profile.js";
 const router = express.Router();
 
-// @route  GET api/posts
-// @desc   Test route
-// @access Public
-router.get("/", (req, res) => {
-  res.send("Post route");
-});
+// @route  POST api/posts
+// @desc   Create a post
+// @access Private
+router.post(
+  "/",
+  [auth, [check("text", "Text is required").not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select("-password");
+
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      });
+
+      const post = await newPost.save();
+
+      res.json(post);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 export default router;
