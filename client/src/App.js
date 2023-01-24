faf153583045aec19dcb3458dca8cd813d459181
Jared Silver <jrsilver88@gmail.com>
2023-01-23 22:23:20 -0800

    added register form html and set up state to handle the form

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,22 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Landing from "./components/layout/Landing";
 import Navbar from "./components/layout/Navbar";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
 const App = () => {
   return (
-    <>
-      <Navbar />
-      <Landing />
-    </>
+    <Router>
+      <>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </>
+    </Router>
   );
 };
 
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,26 +1,29 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCode } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="navbar bg-dark">
-      <h1>
-        <a href="index.html">
-          <FontAwesomeIcon icon={faCode} /> DevConnector
-        </a>
-      </h1>
-      <ul>
-        <li>
-          <a href="profiles.html">Developers</a>
-        </li>
-        <li>
-          <a href="register.html">Register</a>
-        </li>
-        <li>
-          <a href="login.html">Login</a>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <nav className="navbar bg-dark">
+        <h1>
+          <Link to="/">
+            <FontAwesomeIcon icon={faCode} /> DevConnector
+          </Link>
+        </h1>
+        <ul>
+          <li>
+            <Link to="/profiles">Developers</Link>
+          </li>
+          <li>
+            <Link to="/register">Register</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </ul>
+      </nav>
+    </>
   );
 };
 export default Navbar;
